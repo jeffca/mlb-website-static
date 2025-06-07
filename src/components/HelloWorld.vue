d5e5f2bd94f24d60f573ed17a7c0a8161bc71c51
Jeff C <jeff12ca@gmail.com>
2025-06-07 12:51:37 -0500

    first init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+import PlayerTable from './PlayerTable.vue'
+import { ref, onMounted } from 'vue'
+
+const data = ref(null)
+const loading = ref(true)
+
+onMounted(async () => {
+  try {
+    const response = await fetch('/json/pitching-most-h-last-5-days.json')
+    data.value = await response.json()
+    console.log(data.value);
+    loading.value = false
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    loading.value = false
+  }
+})
+</script>
+
+<template>
+  <div>
+    <PlayerTable :data="data" />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/PlayerTable.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  data: Object
+})
+</script>
+
+<template>
+  <!-- <div>{{ props.data }}</div> -->
+
+    <div>
+        <table>
+            <tbody>
+                <tr v-for="row in data">
+                    <td>{{ row.name }}</td>
+                    <td>{{ row.team }}</td>
+                    <td>{{ row.opponent }}</td>
+                    <td>{{ row.metric }}</td>
+                    <td>{{ row.count }}</td>
+                    <td>{{ row.ip.toFixed(1) }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</template>
+
+<style scoped>
+</style>
