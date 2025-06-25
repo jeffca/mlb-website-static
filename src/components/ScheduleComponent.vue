c32a273a273243f1ae662619db72b76746b1fb41
Jeff C <jeff12ca@gmail.com>
2025-06-25 01:24:51 -0500

    dark color scheme

--- src/components/ScheduleComponent.vue
+++ src/components/ScheduleComponent.vue
@@ -31,48 +31,4 @@ onMounted(async () => {
 
 <style scoped>
 
-.today { 
-  font-family:fantasy;
-}
-
-.scoreboard {
-  display: flex;
-  gap: 10px;
-  padding: 10px;
-  overflow-x: auto;
-  white-space: nowrap;
-  background: #f0f0f0;
-}
-
-.game {
-  font-size: 8pt;
-  background: #fff;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  padding: 12px;
-  min-width: 180px;
-  display: flex;
-  flex-direction: column;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-}
-
-.game-box span {
-  margin: 2px 0;
-}
-
-.teams {
-  font-size: smaller;
-  font-family: Helvetica;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  display: block; /* or inline-block */
-  max-width: 100%; /* or a fixed width like 180px */
-  font-weight: bold;
-}
-
-.time {
-  font-size: larger;
-  color: rgb(143, 143, 143);
-}
 </style>
